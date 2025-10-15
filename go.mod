f24261712c501cba19dc83aa60d6e3f46fb0d0b3
Alec Thomas <aat@block.xyz>
2025-10-16 09:20:23 +1100

    feat: initial import
    From https://github.com/block/ftl/tree/main/common/reflect, but originally from
    https://github.com/golang-design/reflect

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/alecthomas/deepcopy
+
+go 1.25.3
+
+require (
+	github.com/alecthomas/assert/v2 v2.2.2
+	github.com/alecthomas/errors v0.8.2
+)
+
+require (
+	github.com/alecthomas/repr v0.2.0 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+)
---
+++ reflect_test.go
@@ -0,0 +1,134 @@
+package deepcopy
+
+import (
+	"container/list"
+	"testing"
+	"time"
+
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestListElements(t *testing.T) {
+	l := list.New()
+	l.PushBack("one")
+	output := Copy(l)
+	assert.Equal(t, output.Front().Value, l.Front().Value)
+	assert.Equal(t, output.Len(), l.Len())
+}
+
+type structOfPointers struct {
+	intPtr    *int
+	floatPtr  *float64
+	structPtr *structOfPointers
+}
+
+func TestCopyStructOfPointers(t *testing.T) {
+	one := 1
+	two := 2
+	half := 0.5
+	quarter := 0.25
+
+	for _, tt := range []struct {
+		name  string
+		input structOfPointers
+	}{
+		{
+			"StructWithinStruct",
+			structOfPointers{
+				intPtr:   &one,
+				floatPtr: &half,
+				structPtr: &structOfPointers{
+					intPtr:    &two,
+					floatPtr:  &quarter,
+					structPtr: nil,
+				},
+			},
+		},
+		{
+			"StructWithinStructWithinStruct",
+			structOfPointers{
+				intPtr:   &one,
+				floatPtr: &half,
+				structPtr: &structOfPointers{
+					intPtr:   &two,
+					floatPtr: &quarter,
+					structPtr: &structOfPointers{
+						intPtr:    &two,
+						floatPtr:  &quarter,
+						structPtr: nil,
+					},
+				},
+			},
+		},
+		{
+			"StructWithNils",
+			structOfPointers{
+				intPtr:    nil,
+				floatPtr:  nil,
+				structPtr: nil,
+			},
+		},
+		{
+			"StructWithStructWithNils",
+			structOfPointers{
+				intPtr:   &one,
+				floatPtr: &half,
+				structPtr: &structOfPointers{
+					intPtr:    nil,
+					floatPtr:  nil,
+					structPtr: nil,
+				},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			output := Copy(tt.input)
+			testEqualityOfStruct(t, &tt.input, &output)
+			// #nosec G601
+		})
+	}
+}
+
+func TestNilTypeCopyIsNil(t *testing.T) {
+	assert.Zero(t, Copy([]int(nil)))
+	assert.Zero(t, Copy(map[string]int(nil)))
+}
+
+func TestDeepCopyTime(t *testing.T) {
+	now := time.Now()
+	assert.Equal(t, now, Copy(now))
+}
+
+func testEqualityOfStruct(t *testing.T, expected, actual *structOfPointers) {
+	t.Helper()
+	if expected == nil || actual == nil {
+		if expected != actual {
+			t.Errorf("struct point does not match nil struct pointer. expected %v, got %v", expected, actual)
+		}
+		return
+	}
+
+	testEqualityOfInt(t, expected.intPtr, actual.intPtr)
+	testEqualityOfFloat64(t, expected.floatPtr, actual.floatPtr)
+	testEqualityOfStruct(t, expected.structPtr, actual.structPtr)
+}
+
+func testEqualityOfInt(t *testing.T, expected, actual *int) {
+	t.Helper()
+	if expected == nil || actual == nil {
+		if expected != actual {
+			t.Errorf("int point does not match nil int pointer. expected %v, got %v", expected, actual)
+		}
+		return
+	}
+}
+
+func testEqualityOfFloat64(t *testing.T, expected, actual *float64) {
+	t.Helper()
+	if expected == nil || actual == nil {
+		if expected != actual {
+			t.Errorf("float point does not match nil float pointer. expected %v, got %v", expected, actual)
+		}
+		return
+	}
+}
